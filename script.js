ec99f4855e67c0b90de2d993db0d22a4e766c02e
Barani203 <92353632+Barani203@users.noreply.github.com>
2021-11-06 08:24:33 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+
+   {
+    "basics": {
+      "name": "Baranidharan.S",
+      "label": "Programmer",
+      "image": "",
+      "email": "naveenkumarmy203@gmail.com",
+      "phone": "7339272237",
+      "url": "https://github.com/",
+      "summary": "A summary of Baranidharan",
+      "location": {
+        "address": "1/9 radhamangalam",
+        "postalCode": "611109",
+        "city": "kilvelur",
+        "countryCode": "in",
+        "region": "india"
+      },
+      "profiles": [{
+        "network": "github",
+        "username": "barani203"
+        "URL": "https://github.com/",
+      }]
+    },
+    
+    {
+    "education": [{
+      "institution": "bharathidasan university",
+      "url": "https://www.google.com/",
+      "area": "Software Development",
+      "studyType": "Bachelor",
+      "startDate": "null",
+      "endDate": "null",
+      "score": "0",
+      "courses": [
+        "DB1101 - Basic SQL"
+      ]
+    }],
+    "awards": [{
+      "title": "Award",
+      "date": "null",
+      "awarder": "null",
+      "summary": "There is no spoon."
+    }],
+    "publications": [{
+      "name": "null",
+      "publisher": "Company",
+      "releaseDate": "null",
+      "url": "https://publication.com",
+      "summary": "Description…"
+    }],
+    "skills": [{
+      "name": "Web Development",
+      "level": "immediter",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages": [{
+      "language": "English",
+      "fluency": "beigner"
+    }],
+    "interests": [{
+      "name": "Wildlife",
+      "keywords": [
+        "Ferrets",
+        "Unicorns"
+      ]
+    }],
+    "references": [{
+      "name": "Baranidharan",
+      "reference": "Reference…"
+    }],
+    "projects": [{
+      "name": "Project",
+      "description": "Description…",
+      "highlights": [
+        "Won award at AIHacks 2016"
+      ],
+      "keywords": [
+        "HTML"
+      ],
+      "startDate": "null",
+      "endDate": "null",
+      "url": "https://project.com/",
+      "roles": [
+        "Team Lead"
+      ],
+      "entity": "Entity",
+      "type": "application"
+    }]
+  }
